92b198188fc50ceb668fb03af0d7292c09c1c340
lorrainejy <lorrainejy@163.com>
2021-05-12 11:27:52 +0800

    feat:部署相关

---
+++ test-serve/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const { createProxyMiddleware }= require('http-proxy-middleware')
+
+app.use(express.static(path.resolve(__dirname,'../dist')))
+
+app.use('/boss', createProxyMiddleware({
+  target: 'http://eduboss.lagou.com',
+  changeOrigin: true
+}))
+
+app.use('/front', createProxyMiddleware({
+  target: 'http://edufront.lagou.com',
+  changeOrigin: true
+}))
+
+app.listen(3000, () => {
+  console.log('listen ....')
+})
